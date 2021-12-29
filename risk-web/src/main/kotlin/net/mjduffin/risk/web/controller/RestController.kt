010849f0d8c88ef571d55359b1ebdf3f477c00e9
Matthew Duffin <mduffin95@users.noreply.github.com>
2021-12-29 21:43:41 +0000

    Refactor and use locks

--- risk-web/src/main/kotlin/net/mjduffin/risk/web/controller/RestController.kt
+++ risk-web/src/main/kotlin/net/mjduffin/risk/web/controller/RestController.kt
@@ -7,6 +7,8 @@ import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 val colors = listOf("red", "blue", "green", "violet", "orange", "magenta", "yellow")
 
@@ -21,50 +23,48 @@ class RestController(val territoryService: TerritoryService, val gameFactory: Ga
         return container.getGameManager()
     }
 
-    @GetMapping("/{id}/game")
-    fun game(@PathVariable("id") id: String): GameVM {
-        return containers[id]?.convert() ?: territoryService.error("No game found for $id")
-    }
-
     @PostMapping("/{id}/draft")
-    fun draft(@PathVariable("id") id: String, @RequestBody draft: Draft): GameVM {
-        val container = containers[id] ?: return territoryService.error("No game found for $id")
-        val gameManager = getGameManager(id) ?: return territoryService.error("No game found for $id")
+    fun draft(@PathVariable("id") id: String, @RequestBody draft: Draft): Response {
+        val container = containers[id] ?: return Response("No game found for $id")
+        val gameManager = getGameManager(id) ?: return Response("No game found for $id")
         val currentState = gameManager.getGameState()
         try {
             gameManager.draftSingle(currentState.currentPlayer, draft.territory, 1)
+            container.increment()
         } catch (ex: Exception) {
         }
-        return container.convert()
+        return Response(null)
     }
 
     @PostMapping("/{id}/attack")
-    fun attack(@PathVariable("id") id: String, @RequestBody attack: Attack): GameVM {
-        val container = containers[id] ?: return territoryService.error("No game found for $id")
-        val gameManager = getGameManager(id) ?: return territoryService.error("No game found for $id")
+    fun attack(@PathVariable("id") id: String, @RequestBody attack: Attack): Response {
+        val container = containers[id] ?: return Response("No game found for $id")
+        val gameManager = getGameManager(id) ?: return Response("No game found for $id")
         val currentState = gameManager.getGameState()
         try {
             gameManager.attack(currentState.currentPlayer, attack.from, attack.to)
+            container.increment()
         } catch (ex: Exception) {
         }
-        return container.convert()
+        return Response(null)
     }
 
     @PostMapping("/{id}/move")
-    fun move(@PathVariable("id") id: String, @RequestBody move: Move): GameVM {
-        val container = containers[id] ?: return territoryService.error("No game found for $id")
-        val gameManager = getGameManager(id) ?: return territoryService.error("No game found for $id")
+    fun move(@PathVariable("id") id: String, @RequestBody move: Move): Response {
+        val container = containers[id] ?: return Response("No game found for $id")
+        val gameManager = getGameManager(id) ?: return Response("No game found for $id")
         val currentState = gameManager.getGameState()
         try {
             gameManager.move(currentState.currentPlayer, move.units)
+            container.increment()
         } catch (ex: Exception) {
         }
-        return container.convert()
+        return Response(null)
     }
 
     @GetMapping("/{id}/end")
-    fun end(@PathVariable("id") id: String): GameVM {
-        val container = containers[id] ?: return territoryService.error("No game found for $id")
+    fun end(@PathVariable("id") id: String): Response {
+        val container = containers[id] ?: return Response("No game found for $id")
         val gameManager = container.getGameManager()
         val currentState = gameManager.getGameState()
         try {
@@ -73,21 +73,23 @@ class RestController(val territoryService: TerritoryService, val gameFactory: Ga
             } else {
                 gameManager.endTurn()
             }
+            container.increment()
         } catch (ex: Exception) {
         }
-        return container.convert()
+        return Response(null)
     }
 
     @PostMapping("/{id}/fortify")
-    fun attack(@PathVariable("id") id: String, @RequestBody fortify: Fortify): GameVM {
-        val container = containers[id] ?: return territoryService.error("No game found for $id")
-        val gameManager = getGameManager(id) ?: return territoryService.error("No game found for $id")
+    fun attack(@PathVariable("id") id: String, @RequestBody fortify: Fortify): Response {
+        val container = containers[id] ?: return Response("No game found for $id")
+        val gameManager = getGameManager(id) ?: return Response("No game found for $id")
         val currentState = gameManager.getGameState()
         try {
             gameManager.fortify(currentState.currentPlayer, fortify.from, fortify.to, fortify.units)
+            container.increment()
         } catch (ex: Exception) {
         }
-        return container.convert()
+        return Response(null)
     }
 
     @GetMapping("/newgame")
@@ -100,48 +102,61 @@ class RestController(val territoryService: TerritoryService, val gameFactory: Ga
     }
 
     @PostMapping("/{id}/join")
-    fun join(@PathVariable("id") id: String, @RequestBody join: Join): LobbyVM {
+    fun join(@PathVariable("id") id: String, @RequestBody join: Join): Response {
 
         val gameContainer = containers[id]
 
         gameContainer?.addPlayer(join.player)
 
-        return LobbyVM(gameContainer?.players ?: listOf(), false,null)
+        return Response(null)
     }
 
-    @PostMapping("/{id}/lobby")
-    fun lobby(@PathVariable("id") id: String, @RequestBody lobby: LobbyVM): LobbyVM {
+    @PostMapping("/{id}/game")
+    fun game(@PathVariable("id") id: String, @RequestBody clientVm: ActionCount): ViewModel {
 
         val start = System.nanoTime()
 
-        var newLobbyVM = lobby
+        var newVm = getViewModel(id)
         while (TimeUnit.SECONDS.convert(System.nanoTime() - start, TimeUnit.MILLISECONDS) < 3_000_000) {
-            Thread.sleep(100)
-            val gameContainer = containers[id]
-            newLobbyVM = LobbyVM(gameContainer?.players ?: listOf(), gameContainer?.hasStarted() ?: false, null)
-            if (newLobbyVM != lobby) {
-                break
+            val c = containers[id]
+            if (c != null) {
+                newVm = c.toViewModel()
+                if (newVm.actionCount > clientVm.actionCount) {
+                    break
+                } else{
+                    // wait for a bit before trying again
+                    c.waitHundredMillis()
+                }
+            } else {
+                return ViewModel(Screen.ERROR, 0, "Container missing for $id")
             }
         }
         System.out.println("Sending: $id")
-        return newLobbyVM
+        return newVm
     }
 
     @GetMapping("/{id}/start")
-    fun start(@PathVariable("id") id: String): GameVM {
+    fun start(@PathVariable("id") id: String): Response {
         val gameContainer = containers[id]!!
         gameContainer.startGame()
-        return gameContainer.convert()
+        return Response(null)
     }
+
+    fun getViewModel(id: String): ViewModel =
+        containers[id]?.toViewModel() ?: ViewModel(Screen.ERROR, 0, "Container not found: $id")
 }
 
 
 class GameContainer(private val gameFactory: GameFactory, private val territoryService: TerritoryService) {
 
     private var manager: GameManager? = null
-    val players: MutableList<Player> = mutableListOf()
+    private val players: MutableList<Player> = mutableListOf()
 
-    var count = 0
+    private val lock = ReentrantLock()
+    private val condition = lock.newCondition()
+
+    var actionCount = 0
+    var playerCount = 0
 
     fun getGameManager(): GameManager {
         if (!hasStarted()) {
@@ -151,18 +166,37 @@ class GameContainer(private val gameFactory: GameFactory, private val territoryS
         }
     }
 
+    fun waitHundredMillis() {
+        lock.withLock { condition.await(100, TimeUnit.MILLISECONDS) }
+    }
+
+    fun increment() {
+        actionCount++
+        lock.withLock { condition.signalAll() }
+    }
+
     fun addPlayer(name: String) = apply {
-        players.add(Player(name, colors[count++]))
+        players.add(Player(name, colors[playerCount++]))
+        increment()
     }
 
     fun startGame() = apply {
         manager = gameFactory.mainGame(players.map { it.name }.toList())
+        increment()
     }
 
-    fun hasStarted(): Boolean = manager != null
+    private fun hasStarted(): Boolean = manager != null
 
+    fun toViewModel(): ViewModel {
+        return if (hasStarted()) {
+            ViewModel(Screen.GAME, actionCount, convert())
+        } else {
+            val lobby = LobbyVM(players)
+            ViewModel(Screen.LOBBY, actionCount, lobby)
+        }
+    }
 
-    fun convert(): GameVM {
+    private fun convert(): GameVM {
         if (!hasStarted()) {
             return error("Game not started")
         }
--- risk-web/src/main/kotlin/net/mjduffin/risk/web/service/ViewModel.kt
+++ risk-web/src/main/kotlin/net/mjduffin/risk/web/service/ViewModel.kt
@@ -1,8 +1,19 @@
 package net.mjduffin.risk.web.service
 
+
+enum class Screen {
+    GAME,
+    LOBBY,
+    ERROR
+}
+
+data class ViewModel(val screen: Screen, val actionCount: Int, val model: Any)
+data class ActionCount(val actionCount: Int)
+
 data class GameVM(val currentPlayer: String, val phase: String, val unitsToPlace: Int, val territories: List<TerritoryVM>, val error: String? = null)
-data class LobbyVM(val players: List<Player>, val hasStarted: Boolean, val error: String?)
+data class LobbyVM(val players: List<Player>)
 data class Player(val name: String, val color: String)
+data class Response(val error: String?)
 
 data class TerritoryVM(val name: String, val top: Int, val left: Int, val player: Player, val units: Int)
 

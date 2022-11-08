e73b4819486c46e7f1f0d3a805405a1eec460374
Matthew Duffin <mduffin95@users.noreply.github.com>
2022-11-08 21:34:33 +0000

    add javadoc

--- risk-lib/src/main/kotlin/net/mjduffin/risk/lib/usecase/request/RequestAcceptor.kt
+++ risk-lib/src/main/kotlin/net/mjduffin/risk/lib/usecase/request/RequestAcceptor.kt
@@ -2,6 +2,9 @@ package net.mjduffin.risk.lib.usecase.request
 
 import net.mjduffin.risk.lib.usecase.GameState
 
+/**
+ * Accepts a request.
+ */
 interface RequestAcceptor {
     fun receiveRequest(request: Request)
 //    fun registerPlayerOutput(output: PlayerOutput)
